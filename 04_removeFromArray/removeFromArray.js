ef8cd51d5646a45854f5a24400ad309da967f462
1-AkM-0 <thepe880@gmail.com>
2025-03-27 14:54:02 -0300

    Add JS solutions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for ( let i = 0 ; i < args.length; i++)
+        {
+            index = array.indexOf(args[i]);
+            last = array.includes(args[i]);
+            if (index !== -1)
+            {
+                while (last === true)
+                {
+                  array.splice(index, 1);
+                  last = array.includes(args[i]); 
+                }
+            }
+        }
+        return array;
 };
 
 // Do not edit below this line
